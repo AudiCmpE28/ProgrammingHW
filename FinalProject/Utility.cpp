7ba81cbf31ca77f0d0ba30354e0ab974f389d47a
dahmedk1999 <71345868+dahmedk1999@users.noreply.github.com>
2021-11-11 20:55:57 -0800

    Update Utility.cpp

--- FinalProject/Utility.cpp
+++ FinalProject/Utility.cpp
@@ -1,12 +1,23 @@
 #include "headers/Utility.h"
 
+using namespace std;
 using namespace std::chrono;
-
+/**
+ * @brief Place before a function. Pass the returned time_point to endtimer function at the end.
+ * 
+ * @return time_point<system_clock> 
+ */
 time_point<system_clock> starttimer()
 {
     auto start= high_resolution_clock::now();
     return start;
 }
+
+/**
+ * @brief Takes the time_point object returned by starttimer. Outputs execution time.
+ * 
+ * @param start 
+ */
 void endtimer(time_point<system_clock> start)
 {
     auto stop= high_resolution_clock::now();
